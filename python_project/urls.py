71842131fe8d16d7b5583def8b153c44900a9314
Yi <mavisluan83@gmail.com>
2019-02-26 19:14:28 -0800

    add template

--- python_project/urls.py
+++ python_project/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.roundtable.urls')),
 
-]
+]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 #
 # urlpatterns = [
